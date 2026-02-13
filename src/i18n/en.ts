9ec710de3122ad3eacc52c4bb9f23137f2e274b4
4i3n6 <4i3n6@pm.me>
2026-02-13 04:14:59 -0300

    chore(version): bump to 1.1.47

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -12,7 +12,7 @@ export const en = {
   // App Header
   header: {
     brand: 'MD2PDF',
-    version: 'v1.1.28',
+    version: 'v1.1.47',
     manualLink: 'Manual',
     manualTitle: 'Open user manual',
   },
--- src/i18n/pt.ts
+++ src/i18n/pt.ts
@@ -14,7 +14,7 @@ export const pt: Translations = {
   // App Header
   header: {
     brand: 'MD2PDF',
-    version: 'v1.1.28',
+    version: 'v1.1.47',
     manualLink: 'Manual',
     manualTitle: 'Abrir manual de uso',
   },
